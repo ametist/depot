2144a930f1fbcf2918ebe0db83a32c8468fa75fe
Vitaliy <vetal_alex@ukr.net>
2012-12-16 23:06:03 +0200

    db data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Emanuel', :city => cities.first)
+
+# Products
+Product.delete_all
+Product.create(
+   :title => 'Meat',
+   :description => %{<p>fasdfasdfasd</p>},
+   :image_url => 'http://sport.ua',
+   :price => 631.3
+)
